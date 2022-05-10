91599f7a27909981879a5f28fe47c7c49e6c30b1
Virendra Summanwar <vsummanwar@gmail.com>
2022-05-10 07:57:48 +0530

    code completed

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from transformers import pipeline
+from pydantic import BaseModel
+
+sentiment_model = pipeline("sentiment-analysis")
+
+class PredictionRequest(BaseModel):
+  query_string: str
 
 app = FastAPI()
 
@@ -6,4 +13,10 @@ app = FastAPI()
 def health():
     return "Service is Online."
 
-    
+@app.post("/performSentimentAnalysis")
+def perform_sentiment_analysis(request: PredictionRequest):
+    # Run sentiment analysis
+    sentiment_query_sentence = request.query_string
+    sentiment = sentiment_model(sentiment_query_sentence) 
+    print(f"Sentiment test: {sentiment_query_sentence} === {sentiment}")
+    return sentiment
